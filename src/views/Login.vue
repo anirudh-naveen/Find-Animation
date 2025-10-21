d9d218d233a54bfa1575ee4e46a2633cb38f1ed0
anirudhnaveen@outlook.com <anirudhnaveen@outlook.com>
2025-10-21 11:06:11 -0400

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -76,7 +76,7 @@ const handleLogin = async () => {
   try {
     await authStore.login(form.value)
     toast.success('Login successful!')
-    
+
     // Wait for next tick to ensure auth state is updated
     await new Promise((resolve) => setTimeout(resolve, 100))
 
